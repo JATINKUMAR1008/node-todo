ff284a8bf011b5fe25068cf4654a1fdb8eab3c33
JATINKUMAR1008 <97216852+JATINKUMAR1008@users.noreply.github.com>
2023-01-14 17:37:10 +0530

    ok

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    remoteUrl : 'mongodb+srv://Jatin:suntu%401008@cluster0.13xzk.mongodb.net/?retryWrites=true&w=majority',
+    localUrl: 'mongodb+srv://Jatin:suntu%401008@cluster0.13xzk.mongodb.net/?retryWrites=true&w=majority'
+};
